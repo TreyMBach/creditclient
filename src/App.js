e92a374583f00a0b1c48077ba635f287a2df4551
Trey Bach <64717193+TreyMBach@users.noreply.github.com>
2023-11-08 15:37:36 -0700

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, {useState} from 'react';
+import axios from 'axios';
 
 function App() {
 
@@ -8,10 +9,30 @@ function App() {
   const [age, setAge] = useState(0);
   const [credit, setCredit] = useState(0);
 
-  console.log(name, age, credit)
+  const [peopleList, setpeopleList] = useState([])
+
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    axios.post('https://credit-form-d9821669364c.herokuapp.com/create', {
+      name: name, 
+      age: age, 
+      credit: credit
+    }).then(() => {
+      console.log("Success")
+    })
+  }
+
+  const showPeople = (event) => {
+    event.preventDefault();
+    axios.get('https://credit-form-d9821669364c.herokuapp.com/people').then((response) => {
+      setpeopleList(response.data)
+      console.log(response.data)
+    })
+  }
+
   return (
     <div>
-
           <header>
               Credit Form
           </header> 
@@ -37,7 +58,21 @@ function App() {
                   value={credit} 
                   onChange={e => setCredit(e.target.value)} />
               </label>
+              <button onClick={addPerson}>Add Person</button>
           </form>
+          <hr />
+          <div className='people'>
+            <button onClick={showPeople}>Show People</button>
+
+            {peopleList.map((val, key) => {
+                return <div className='person'>
+                    <h3>Name: {val.name}</h3>
+                    <h3>Age: {val.age}</h3>
+                    <h3>Credit: {val.credit}</h3>
+                </div>
+            })}
+          </div>
+
 
     </div>
   );
